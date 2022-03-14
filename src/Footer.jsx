01eaf883983d63fb67854c3504d73fa0b8d61aa9
LauraMooney <79043901+LauraMooney@users.noreply.github.com>
2022-03-14 20:45:39 +0000

    updated footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,30 +1,18 @@
 import React from 'react';
 import './App';
 import './App.css';
-import { Container, Nav, } from "reactstrap";
 
 function Footer() {
     return (
       <footer className="footer">
-      <Container fluid>
-        <Nav>
           <p className='footercontent'>
-              Created using OMDb API and react
-          </p>
-         
-        </Nav>
+              Created using IMDb API and React
+          </p>   
         <div className="copyright">
           © {new Date().getFullYear()} made by{" "}
-          <i className="tim-icons icon-heart-2" /> {" "}
-          <a
-            href="https://www.purpleladder.co.uk"
-            target="_blank"
-          >
+          <i className="tim-icons icon-heart-2" />
             Laura Mooney
-          </a>{" "}
-          Digital Business Solutions.
         </div>
-      </Container>
     </footer>
     );
   }
